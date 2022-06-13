65b52d9a47e8fe3781c49f1b3ec268c30e4b6e6b
Rohit Kumar Shah <rohitshah4083@gmail.com>
2022-06-13 22:43:08 +0530

    Adding whack-a-mole game

---
+++ whack-a-mole/app.js
@@ -0,0 +1,51 @@
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
+
+let result = 0;
+let hitPosition;
+let currentTime = 15;
+let timer = null;
+
+function randomSquare(){
+    squares.forEach(square => {
+        square.classList.remove('mole');
+    })
+    let randomSquare = squares[Math.floor(Math.random() * 9)];
+    randomSquare.classList.add('mole');
+    hitPosition = randomSquare.id;
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id === hitPosition){
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    })
+})
+
+function moveMole(){
+    timer = setInterval(() => {
+        randomSquare();
+    }, 800);
+}
+
+function countDown(){
+    currentTime--;
+    timeLeft.textContent = currentTime;
+    if(currentTime === 0){
+        clearInterval(countDownTimer);
+        clearInterval(timer);
+        alert('Game Over and Your Score is : ' + result);
+    }
+
+}
+
+let countDownTimer = setInterval(() => {
+    countDown();
+}, 1000);
+
+moveMole();
